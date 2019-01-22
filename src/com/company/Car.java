12ed02fbecd3e8b27e63744338e7b098eef330a0
Joseph Double <josephdouble@Josephs-MacBook-Pro.local>
2019-01-22 13:56:41 -0500

    Creating the accessor methods for each variable.

--- src/com/company/Car.java
+++ src/com/company/Car.java
@@ -6,6 +6,28 @@ public class Car {
     private String model;
     private String color;
 
+    public void setMake(String carMake) {
+        make = carMake;
+    }
 
+    public String getMake() {
+        return make;
+    }
 
+    public void setModel(String carModel) {
+        model = carModel;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setColor(String carColor) {
+        color = carColor;
+    }
+
+    public String getColor() {
+        return color;
+    }
+    
 }
