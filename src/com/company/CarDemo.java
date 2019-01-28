058afecbef01e29ff95e240ae3a62147c10bf156
Joseph Double <josephdouble@Josephs-MacBook-Pro.local>
2019-01-28 15:08:56 -0500

    Cleaned up formatting of my code.

--- src/com/company/CarDemo.java
+++ src/com/company/CarDemo.java
@@ -4,15 +4,15 @@ public class CarDemo {
 
     public static void main(String[] args) {
 
-    Car myCar = new Car();
+        Car myCar = new Car();
 
-     myCar.setColor("Black");
-     myCar.setMake("Ford");
-     myCar.setModel("Escape");
+        myCar.setColor("Black");
+        myCar.setMake("Ford");
+        myCar.setModel("Escape");
 
-     System.out.println("My car is colored " + myCar.getColor());
-     System.out.println("The make of my car is " + myCar.getMake());
-     System.out.println("The model of my car is " + myCar.getModel());
+        System.out.println("My car is colored " + myCar.getColor());
+        System.out.println("The make of my car is " + myCar.getMake());
+        System.out.println("The model of my car is " + myCar.getModel());
 
     }
 }
